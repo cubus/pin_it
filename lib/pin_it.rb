3cac790fb59e65fc3f4f0361e28e2a35a209bc87
Matenia Rossides <matenia@gmail.com>
2012-07-07 14:59:29 +1000

    added option to provide count_layout param to pin it button

--- lib/pin_it.rb
+++ lib/pin_it.rb
@@ -10,10 +10,11 @@ module PinIt
     def pin_it_button(options = {})
       %{<a href="http://pinterest.com/pin/create/button/?url=page_url&media=img_url&description=description" class="pin-it-button" count-layout="horizontal">Pin It</a>}
       query_params = options.slice(:url, :media, :description)
+      layout_params = options[:count_layout]
       img = tag :img, :src => "//assets.pinterest.com/images/PinExt.png", :title => "Pin It", :border => "0" 
       content_tag :a, img, "href" => "http://pinterest.com/pin/create/button/?#{query_params.to_query}", 
                                 "class" => "pin-it-button",
-                                "count-layout" => "horizontal"
+                                "count-layout" => layout_params || "horizontal"
     end
     
     def pin_it_js
