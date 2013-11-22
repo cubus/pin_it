c945c338ff3df4eb33b425c5ecc3ff65872813f0
Mihai Târnovan <mihai.tarnovan@cubus.ro>
2013-11-22 22:55:58 +0200

    Allow changing pin color

--- lib/pin_it.rb
+++ lib/pin_it.rb
@@ -7,16 +7,21 @@ end
 
 module PinIt
   module Helper
+
+    PIN_ICON_URL_TEMPLATE = "//assets.pinterest.com/images/pidgets/pinit_fg_en_rect_%s_20.png"
+
     def pin_it_button(options = {})
       %{<a href="http://pinterest.com/pin/create/button/?url=page_url&media=img_url&description=description" class="pin-it-button" count-layout="horizontal">Pin It</a>}
       query_params = options.slice(:url, :media, :description)
-      layout_params = options[:count_layout]
-      img = tag :img, :src => "//assets.pinterest.com/images/PinExt.png", :title => "Pin It", :border => "0" 
-      content_tag :a, img, "href" => "http://pinterest.com/pin/create/button/?#{query_params.to_query}", 
+      pin_color = options[:pin_color] || :gray
+      pin_config = options[:pin_config] || :horizontal
+      img = tag :img, :src => PIN_ICON_URL_TEMPLATE % pin_color, :title => "Pin It", :border => "0"
+      content_tag :a, img, "href" => "http://pinterest.com/pin/create/button/?#{query_params.to_query}",
                                 "class" => "pin-it-button",
-                                "count-layout" => layout_params || "horizontal"
+                                data: { "pin-color" => pin_color, "pin-config" => pin_config }
     end
-    
+
+
     def pin_it_js
       @pin_it_js ||= IO.read(File.expand_path("../../vendor/assets/javascripts/pin_it.js", __FILE__))
     end
